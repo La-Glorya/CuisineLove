6933fb4f8d66496cf199bf4569e525066ccaca66
amina78 <43169533+amina78@users.noreply.github.com>
2020-06-23 13:18:47 +0200

    Add files via upload

---
+++ Cuisine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using cuisine.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace cuisine.dal
+{
+    public class Cuisine: DbContext
+    {
+        public Cuisine() : base("Cuisine") { }
+        public DbSet<Recette> Recettes { get; set; }
+        public DbSet<Plat> Plats { get; set; }
+        public DbSet<Ingredient> Ingrediens { get; set; }
+        public DbSet<Preparation> Preparations { get; set; }
+        public DbSet<Pathologie> Pathologies { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+     
+    }
+
+}
